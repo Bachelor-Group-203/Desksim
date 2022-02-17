dd268a25fa4bec0d5b86a7eaa926baaed99af294
Marco <62345298+MarcoipPolo@users.noreply.github.com>
2022-02-17 09:08:38 +0100

    Adding some future features as comments for after controller fix

--- Assets/Scripts/TrainScripts/TrainValues.cs
+++ Assets/Scripts/TrainScripts/TrainValues.cs
@@ -10,4 +10,11 @@ public class TrainValues : MonoBehaviour
     [SerializeField] public float maxAcceleration = 5.0f;           // default 5 m/s
     [Header("Breaking force of the train in N")]
     [SerializeField] public float maxBreakForce = 50.0f;            // default 50 N
+    [Header("Max velocity of the train in km/h")]
+    [SerializeField] public float maxVelocity = 120.0f;
+
+    public float GetMaxVelocityAsMS()
+    {
+        return (maxVelocity / 3.6f);
+    }
 }
--- Assets/Scripts/TrainScripts/Traincontroller.cs
+++ Assets/Scripts/TrainScripts/Traincontroller.cs
@@ -8,6 +8,10 @@ public class Traincontroller : MonoBehaviour
     private TrainValues tValues;
     private Rigidbody rigidbody;
 
+
+    // TODO!!! Find out how to get the train head direction
+    private Vector3 tDirection = new Vector3(1, 0, 0);
+
     private void Awake()
     {
         tValues = GetComponent<TrainValues>();
@@ -28,6 +32,14 @@ public class Traincontroller : MonoBehaviour
          ******************/
         // Breaks go from 0 to 5 bar
         // controller 0 - 100% * maxBreakForce = current BreakForce
+
+        /******************
+         * Notes and uefull things
+         ******************/
+
+        // For tracking the speed of the train in km/h
+        Debug.Log(Vector3.Magnitude(rigidbody.velocity) * 3.6 + " km/h");
+
     }
 
     // FixedUpdate is called many times per frame
@@ -36,11 +48,21 @@ public class Traincontroller : MonoBehaviour
         /***************************
          * Execute train acceleraton
          ***************************/
-        rigidbody.AddForce(tValues.mass * tValues.maxAcceleration, 0, 0, ForceMode.Force);
+        if (Vector3.Magnitude(rigidbody.velocity) >= tValues.GetMaxVelocityAsMS())
+        {
+            //Debug.LogWarning("Train exceeds the speedlimit!!!");
+            rigidbody.velocity = tDirection.normalized * tValues.GetMaxVelocityAsMS();
+            rigidbody.AddForce(0, 0, 0);
+        }
+        else
+        {
+            rigidbody.AddForce(tValues.maxAcceleration, 0, 0);
+        }
 
         /**********************
          * Execute train breaks
          **********************/
-        rigidbody.AddForce(-tValues.maxBreakForce, 0, 0, ForceMode.Force);
+        //rigidbody.AddForce(-tValues.maxBreakForce, 0, 0, ForceMode.Force);
+
     }
 }
