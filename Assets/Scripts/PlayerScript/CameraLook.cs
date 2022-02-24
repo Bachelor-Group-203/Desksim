f67d66703b66aa67e507dcb90c69341faf10c20f
Marco <62345298+MarcoipPolo@users.noreply.github.com>
2022-02-24 19:43:00 +0100

    Implemented a height map

--- Assets/Scripts/PlayerScript/CameraLook.cs
+++ Assets/Scripts/PlayerScript/CameraLook.cs
@@ -19,9 +19,9 @@ public class CameraLook : MonoBehaviour
     void Update()
     {
         // TODO!!! Make new inputsystem work with this
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        playerBody.Rotate(Vector3.up * mouseX );
+        //playerBody.Rotate(Vector3.up * mouseX );
     }
 }
